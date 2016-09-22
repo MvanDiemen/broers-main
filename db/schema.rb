cd23b495922fd393d7f5869b3f26a544285e61cf
Michaël van Diemen <van.diemen.michael@gmail.com>
2016-09-22 15:57:45 +0200

    Added number to order

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913164709) do
+ActiveRecord::Schema.define(version: 20160922133920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20160913164709) do
     t.string   "lens_type"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "number"
   end
 
   add_index "orders", ["customer_id"], name: "index_orders_on_customer_id", using: :btree
