c99fa0c3c880b28566f4e082e745ac8ab4952778
Michaël van Diemen <van.diemen.michael@gmail.com>
2016-09-24 17:45:38 +0200

    Subscriptions now showing

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,25 +1,9 @@
 class SubscriptionsController < ApplicationController
   def index
-
+    @subscriptions = Subscription.includes(order: :customer).all
   end
 
   def show
-
-  end
-
-  def new
-
-  end
-
-  def create
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
+    @subscritpion.find(params[:id])
   end
 end
